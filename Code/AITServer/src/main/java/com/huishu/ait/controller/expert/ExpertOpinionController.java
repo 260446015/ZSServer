326bd5d966a86d4075fe2f0688ef5609783cffac
yangxuqiang <646160127@qq.com>
2017-08-02 15:58:35 +0800

    合并专家controller

--- Code/AITServer/src/main/java/com/huishu/ait/controller/expert/ExpertOpinionController.java
+++ Code/AITServer/src/main/java/com/huishu/ait/controller/expert/ExpertOpinionController.java
@@ -1,5 +1,7 @@
 package com.huishu.ait.controller.expert;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +15,27 @@ import com.alibaba.fastjson.JSONObject;
 import com.huishu.ait.common.conf.MsgConstant;
 import com.huishu.ait.common.util.ConcersUtils;
 import com.huishu.ait.controller.BaseController;
+import com.huishu.ait.entity.Specialist;
 import com.huishu.ait.entity.common.AjaxResult;
 import com.huishu.ait.entity.dto.GardenDTO;
 import com.huishu.ait.es.entity.ExpertOpinionDTO;
 import com.huishu.ait.service.ExpertOpinion.ExpertOpinionService;
+import com.huishu.ait.service.Specialist.SpecialistService;
 
 /**
  * @author yxq
  *	查询专家观点
  */
 @RestController
-@RequestMapping(value = "expertOpinion")
+@RequestMapping(value = "expert")
 public class ExpertOpinionController extends BaseController{
 	
 	private static Logger log = LoggerFactory.getLogger(ExpertOpinionController.class);
 	
 	@Autowired
 	private ExpertOpinionService expertOpinionService;
+	@Autowired
+	private SpecialistService specialistService;
 	
 	/**
 	 * @param dto
@@ -47,7 +53,16 @@ public class ExpertOpinionController extends BaseController{
 		}
 		return dto;
 	}
-	
+	@RequestMapping(value = "getSpecialist.json")
+	public AjaxResult getSpecialist(){
+		try {
+			List<Specialist> findAll = specialistService.findAll();
+			return success(findAll);
+		} catch (Exception e) {
+			log.error("查询失败：",e.getMessage());
+			return error(MsgConstant.ILLEGAL_PARAM);
+		}
+	}
 	/**
 	 * @param requestParam
 	 * @return
