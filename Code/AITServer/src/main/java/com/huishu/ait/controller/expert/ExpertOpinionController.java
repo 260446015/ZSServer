b9b943e97c42456d904b021fc405ddc5befbc606
yangxuqiang <646160127@qq.com>
2017-08-02 15:41:34 +0800

    控制层url修改，收藏和取消专家观点，

--- Code/AITServer/src/main/java/com/huishu/ait/controller/expert/ExpertOpinionController.java
+++ Code/AITServer/src/main/java/com/huishu/ait/controller/expert/ExpertOpinionController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONArray;
@@ -24,7 +23,7 @@ import com.huishu.ait.service.ExpertOpinion.ExpertOpinionService;
  *	查询专家观点
  */
 @RestController
-@RequestMapping(value = "/ExpertOpinion")
+@RequestMapping(value = "expertOpinion")
 public class ExpertOpinionController extends BaseController{
 	
 	private static Logger log = LoggerFactory.getLogger(ExpertOpinionController.class);
@@ -32,11 +31,15 @@ public class ExpertOpinionController extends BaseController{
 	@Autowired
 	private ExpertOpinionService expertOpinionService;
 	
+	/**
+	 * @param dto
+	 * 初始化分页的方法
+	 */
 	private ExpertOpinionDTO initPage(ExpertOpinionDTO dto){
-		if(null == dto.getPageNumber()){
-			dto.setPageNumber(ConcersUtils.ES_MIN_PAGENUMBER);
+		if(dto.getPageNumber() == null){
+			dto.setPageNumber(1);
 		}
-		if(null == dto.getPageSize()){
+		if(dto.getPageSize() == null){
 			dto.setPageSize(ConcersUtils.PAGE_SIZE);
 		}
 		if(dto.getPageNumber() > ConcersUtils.ES_MAX_PAGENUMBER){
@@ -44,48 +47,85 @@ public class ExpertOpinionController extends BaseController{
 		}
 		return dto;
 	}
-	@RequestMapping(value = "findaExpertOpinion.json",method= RequestMethod.POST)
+	
+	/**
+	 * @param requestParam
+	 * @return
+	 * 查询专家观点
+	 */
+	@RequestMapping(value = "findaExpertOpinion.json")
 	public AjaxResult getExpertOpinion(ExpertOpinionDTO requestParam){
 		try {
 			requestParam = initPage(requestParam);
 			JSONArray jsonArray = expertOpinionService.getExertOpinionList(requestParam);
 			return this.success(jsonArray);
 		} catch (Exception e) {
-			log.error("查询失败：",e);
+			log.error("查询失败：",e.getMessage());
 			return error(MsgConstant.ILLEGAL_PARAM);
 		}
 	}
-	@RequestMapping(value = "/findExpertOpinionByAuthor.json",method=RequestMethod.POST)
+	/**
+	 * @param requestParam
+	 * @return
+	 * 根据作者名称查询观点列表
+	 */
+	@RequestMapping(value = "findExpertOpinionByAuthor.json")
 	public AjaxResult getExpertOpinionByAuthor(ExpertOpinionDTO requestParam){
 		try {
 			requestParam = initPage(requestParam);
 			JSONArray jsonArray = expertOpinionService.findExpertOpinionByAuthor(requestParam);
 			return this.success(jsonArray);
 		} catch (Exception e) {
-			log.error("查询失败：",e);
+			log.error("查询失败：",e.getMessage());
 			return error(MsgConstant.ILLEGAL_PARAM);
 		}
 	}
-	@RequestMapping(value = "/findExpertOpinionById.json", method=RequestMethod.POST)
+	/**
+	 * @param id
+	 * @return
+	 * 根据ID查询专家观点详情
+	 */
+	@RequestMapping(value = "findExpertOpinionById.json")
 	public AjaxResult getExpertOpinionById(String id){
 		try {
 			JSONObject json = expertOpinionService.findExpertOpinionById(id);
 			return this.success(json);
 		} catch (Exception e) {
-			log.error("查询失败：",e);
+			log.error("查询失败：",e.getMessage());
 			return error(MsgConstant.ILLEGAL_PARAM);
 		}
 	}
-	
-	@RequestMapping(value = "collectExpertOpinion.json", method= RequestMethod.POST)
-	public AjaxResult collectExpertOpinion(@RequestBody ExpertOpinionDTO requestParam){
+	/**
+	 * @param requestParam
+	 * @return
+	 * 收藏专家观点
+	 */
+	@RequestMapping(value = "collectExpertOpinion.json")
+	public AjaxResult collectExpertOpinion(@RequestBody String id){
 		try {
-			Boolean flag = expertOpinionService.expertOpinionCollect(requestParam);
+			Boolean flag = expertOpinionService.expertOpinionCollect(id);
 			AjaxResult result = new AjaxResult();
 			result.setSuccess(flag);
 			return result;
 		} catch (Exception e) {
-			log.error("收藏失败：",e);
+			log.error("收藏失败：",e.getMessage());
+			return null;
+		}
+	}
+	/**
+	 * @param requestParam
+	 * @return
+	 * 取消收藏专家观点
+	 */
+	@RequestMapping(value = "cancelCollectExpertOpinion.json")
+	public AjaxResult cancelCollectExpertOpinion(@RequestBody String id){
+		try {
+			Boolean flag = expertOpinionService.cancelExpertOpinionCollect(id);
+			AjaxResult result = new AjaxResult();
+			result.setSuccess(flag);
+			return result;
+		} catch (Exception e) {
+			log.error("取消收藏失败：",e.getMessage());
 			return null;
 		}
 	}
