e0eed360283668386cce9fd83d501922dda2999d
260446015 <260446015@qq.com>
2017-08-14 17:56:03 +0800

    今日头条保存用户所选产业标签

--- Code/AITServer/src/main/java/com/huishu/ait/controller/param/BaseParamController.java
+++ Code/AITServer/src/main/java/com/huishu/ait/controller/param/BaseParamController.java
@@ -1,7 +1,10 @@
 package com.huishu.ait.controller.param;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +16,7 @@ import com.huishu.ait.controller.BaseController;
 import com.huishu.ait.entity.Param;
 import com.huishu.ait.entity.common.AjaxResult;
 import com.huishu.ait.service.param.ParamService;
+import com.alibaba.fastjson.JSONObject;
 import com.huishu.ait.common.conf.ConfConstant;
 import com.huishu.ait.common.conf.ConfConstant.INDUSTRY;
 /**
@@ -28,6 +32,8 @@ public class BaseParamController extends BaseController {
 	
 	@Autowired
 	private ParamService  service;
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(BaseParamController.class);
 	
 	//首次进入今日头条，将会给予提示
 	@ResponseBody
@@ -48,8 +54,28 @@ public class BaseParamController extends BaseController {
 		}
 		boolean b = false;
 		String[] msg = param.getMsg();
+		List<Param> list = new ArrayList<>();
 		for (int i = 0; i < msg.length; i++) {
-			
+			Param pa = new Param();
+			JSONObject obj = JSONObject.parseObject(msg[i]);
+			for (String str : obj.keySet()) {
+				pa.setIndustryInfo(str);
+			}
+			pa.setIndustryLagel(obj.getString(pa.getIndustryInfo()));
+			pa.setId(userId);
+			list.add(pa);
+		}
+		try{
+			b = service.saveParams(list);
+		}catch(Exception e){
+			LOGGER.error("存储今日头条爱好条件出错",e);
+			return error("存储今日头条爱好条件出错");
+		}
+		if(!b){
+			return error("保存失败！");
+		}else{
+			List<Param> list2 = service.findByUid(userId);
+			return success(list2);
 		}
 		/*for (Param param : list) {
 			param.setUid(userId);
@@ -62,7 +88,6 @@ public class BaseParamController extends BaseController {
 			List<Param> list2 = service.findByUid(userId);
 			return success(list2);
 		}*/
-		return error(MsgConstant.ILLEGAL_PARAM);
 	}
 	/**
 	 * 根据当前的用户id 查询所有的参数信息--用于页面跳转时使用
