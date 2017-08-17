422d0b9c7f6ee3fd1296ab45213f69102544a532
260446015 <260446015@qq.com>
2017-08-17 11:12:36 +0800

    提交天眼查的repository

---
+++ Code/AITServer/src/main/java/com/huishu/ait/repository/skyeye/SkyEyeRepository.java
@@ -0,0 +1,9 @@
+package com.huishu.ait.repository.skyeye;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.huishu.ait.entity.SkyEyeAuthEntity;
+
+public interface SkyEyeRepository extends CrudRepository<SkyEyeAuthEntity, Long>{
+
+}
