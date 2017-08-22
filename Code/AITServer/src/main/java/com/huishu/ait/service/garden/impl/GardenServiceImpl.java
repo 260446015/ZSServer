ea4a05e717311240815435dff1a5fb3adaed82c0
260446015 <260446015@qq.com>
2017-08-22 14:52:14 +0800

    提交按地区查询园区返回值得代码

--- Code/AITServer/src/main/java/com/huishu/ait/service/garden/impl/GardenServiceImpl.java
+++ Code/AITServer/src/main/java/com/huishu/ait/service/garden/impl/GardenServiceImpl.java
@@ -196,68 +196,7 @@ public class GardenServiceImpl extends AbstractService implements GardenService 
             return null;
         }
     }
-	/* 园区情报中获取所有园区内容
-	 * @see com.huishu.ait.service.garden.GardenService#findGardensAll()
-	 */
-	@Override
-	public JSONArray findGardensAll() {
-		JSONArray data = new JSONArray();
-		try{
-			Iterable<GardenData> findAll = gardenRepository.findAll();
-			JSONArray arr1 = new JSONArray();//动漫产业
-			JSONArray arr2 = new JSONArray();//影视产业
-			JSONArray arr3 = new JSONArray();//生态科技
-			JSONArray arr4 = new JSONArray();//生物药业
-			JSONArray arr5 = new JSONArray();//信息技术
-			for (GardenData garden : findAll) {
-				JSONObject obj = new JSONObject();
-//				obj.put("id", garden.getId());
-//				obj.put("area", garden.getArea());
-//				obj.put("gardenType", garden.getGardenType());
-				obj.put("name", garden.getGardenName());
-				int value = 0;
-				switch (garden.getGardenType()) {
-				case "动漫产业":
-					value = 0;
-					break;
-				case "影视产业":
-					value = 2;
-					break;
-				case "生态科技":
-					value = 3;
-					break;
-				case "生物药业":
-					value = 4;
-					break;
-				case "信息技术":
-					value = 5;
-					break;
-				default:
-					break;
-				}
-				obj.put("value", value);
-				if(garden.getGardenType().equals("动漫产业")){
-					arr1.add(obj);
-				}else if(garden.getGardenType().equals("影视产业")){
-					arr2.add(obj);
-				}else if(garden.getGardenType().equals("生态科技")){
-					arr3.add(obj);
-				}else if(garden.getGardenType().equals("生物药业")){
-					arr4.add(obj);
-				}else if(garden.getGardenType().equals("信息技术")){
-					arr5.add(obj);
-				}
-			}
-			data.add(arr1);
-			data.add(arr2);
-			data.add(arr3);
-			data.add(arr4);
-			data.add(arr5);
-		}catch(Exception e){
-			LOGGER.error("获取园区情报中获取所有园区内容失败",e);
-		}
-		return data;
-	}
+	
 	/* 此处实现关注园区的功能
 	 */
 	@Override
@@ -325,9 +264,7 @@ public class GardenServiceImpl extends AbstractService implements GardenService 
 			List<GardenData> list = gardenRepository.findGardensByArea(area);
 			for (GardenData garden : list) {
 				JSONObject obj = new JSONObject();
-				obj.put("address", garden.getAddress());
-				obj.put("name", garden.getGardenName());
-				obj.put("industryType", garden.getLeadingIndustry());
+				obj.put(garden.getGardenName(), garden.getAddress());
 				arr.add(obj);
 			}
 		}catch(Exception e){
