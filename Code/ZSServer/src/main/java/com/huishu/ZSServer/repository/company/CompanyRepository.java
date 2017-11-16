6a7ccdcab9a818ac5ade3703a3a7209b7b61402f
260446015 <260446015@qq.com>
2017-11-16 10:18:45 +0800

    提交企业dao

--- Code/ZSServer/src/main/java/com/huishu/ZSServer/repository/company/CompanyRepository.java
+++ Code/ZSServer/src/main/java/com/huishu/ZSServer/repository/company/CompanyRepository.java
@@ -2,8 +2,6 @@ package com.huishu.ZSServer.repository.company;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -12,9 +10,6 @@ import com.huishu.ZSServer.entity.Company;
 
 public interface CompanyRepository extends CrudRepository<Company, Long>, JpaSpecificationExecutor<Company> {
 
-	@Query(value = "from Company c where c.industry like ?1 and c.industryLabel like ?2 and c.scale between ?3 and ?4 and c.registerDate between ?5 and ?6 and c.registerCapital between ?7 and ?8 and c.invest like ?9")
-	Page<Company> findCompanyList(String industry, String industryLable, Integer scale, Integer ecale, String startTime,
-			String endTime, Double sregist, Double eregist, String invest, Pageable page);
 
 	/**
 	 * @param area
