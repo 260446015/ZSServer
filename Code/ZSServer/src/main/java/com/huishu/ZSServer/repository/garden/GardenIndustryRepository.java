497072f97cfc62c69c74743f72438a07e6967851
260446015 <260446015@qq.com>
2017-11-27 18:42:54 +0800

    提交代码

---
+++ Code/ZSServer/src/main/java/com/huishu/ZSServer/repository/garden/GardenIndustryRepository.java
@@ -0,0 +1,9 @@
+package com.huishu.ZSServer.repository.garden;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.huishu.ZSServer.entity.garden.GardenIndustry;
+
+public interface GardenIndustryRepository extends CrudRepository<GardenIndustry, Long>{
+
+}
