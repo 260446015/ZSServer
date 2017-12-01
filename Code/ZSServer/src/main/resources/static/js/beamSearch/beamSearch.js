997c1d2ec21801ab7e89e5a338bd27d4564a1764
yindongqing <572213080@qq.com>
2017-12-01 17:51:31 +0800

    修改侧边栏

--- Code/ZSServer/src/main/resources/static/js/beamSearch/beamSearch.js
+++ Code/ZSServer/src/main/resources/static/js/beamSearch/beamSearch.js
@@ -96,7 +96,7 @@ function showCompany(list){
                '<p>注册资金:<span class="company-money">'+obj.regCapital+'</span></p>'+
                '<p>总部地点:<span class="company-address">'+obj.base+'</span></p>'+
                '<div class="company-mask"><div class="circle-empty"><img src="/images/see_icon.png" />'+
-               '<a href="javascript:void(0);" class="text-center">查看详情</a></div></div></div></div></div>';
+               '<a href="/apis/area/company/companyDetail.html?companyName='+obj.name+'" class="text-center">查看详情</a></div></div></div></div></div>';
            }
            $("#city_list").html(cHtml);//将数据增加到页面中
        }
