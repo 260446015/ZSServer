d97ba7c467c5df15a3b14f6d9ad00bed71a1f56a
yindongqing <572213080@qq.com>
2017-12-28 18:57:51 +0800

    开放拦截

--- Code/ZSServer/src/main/java/com/huishu/ZSServer/app/conf/ShiroConfiguration.java
+++ Code/ZSServer/src/main/java/com/huishu/ZSServer/app/conf/ShiroConfiguration.java
@@ -47,8 +47,8 @@ public class ShiroConfiguration {
 
 		filterChainDefinitionMap.put("/login.html", "anon");
 
-//		filterChainDefinitionMap.put("/**/*.html", "authc");
-//		filterChainDefinitionMap.put("/**/*.json", "authc");
+		filterChainDefinitionMap.put("/**/*.html", "authc");
+		filterChainDefinitionMap.put("/**/*.json", "authc");
 
 		shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
 
