7d90750f0ef5040ff0f85b42e9b8634c27301fd7
yindongqing <572213080@qq.com>
2018-01-20 17:52:44 +0800

    去掉一个删除

--- Code/AITManagerServer/src/main/java/com/huishu/ait/app/conf/TaskConfiguration.java
+++ Code/AITManagerServer/src/main/java/com/huishu/ait/app/conf/TaskConfiguration.java
@@ -73,8 +73,7 @@ public class TaskConfiguration {
 					substring+=over.substring(indexOf2,over.length());
 				}
 				if(substring.length()==0){
-					baseElasticsearch.delete(ait.getId());
-					LOGGER.info("对象{}没有找到在内容中找到时间相关，删除",ait.getId());
+					LOGGER.info("对象{}没有找到在内容中找到时间相关");
 					continue;
 				}
 				int titleIndex = title1.indexOf("201");
