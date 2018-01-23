f7cec9cf468053676f4dce3d65670d77c631db83
qingyun <1348426268@qq.com>
2018-01-23 18:02:56 +0800

    提交测试

---
+++ Code/ZSServer/src/test/java/industry/InduTest.java
@@ -0,0 +1,24 @@
+package industry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.alibaba.fastjson.JSONObject;
+import com.forget.analysis.Analysis;
+import com.huishu.ZSServer.app.Application;
+import com.huishu.ZSServer.es.repository.BaseElasticsearch;
+
+/**
+ * @author hhy
+ * @date 2017年11月9日
+ * @Parem
+ * @return 
+ * 
+ */
+public class InduTest {
+  
+}
