d3d18259a1a0b35ccb2a94e43d0a28f1127f82f0
huhaiyang <1208027930@qq.com>
2018-01-23 17:54:55 +0800

    提交后台文章管理

---
+++ Code/ManageServer/src/main/java/com/huishu/ManageServer/service/keyword/impl/KeyWordServiceImpl.java
@@ -0,0 +1,66 @@
+package com.huishu.ManageServer.service.keyword.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.huishu.ManageServer.entity.dbFirst.KeyWordEntity;
+import com.huishu.ManageServer.repository.first.KeyWordRepository;
+import com.huishu.ManageServer.service.keyword.KeyWordService;
+
+
+/**
+ * @author hhy
+ * @date 2017年12月19日
+ * @Parem
+ * @return 
+ * 关键词云实现方法
+ */
+@Service
+public class KeyWordServiceImpl implements KeyWordService {
+	
+	@Autowired
+	private KeyWordRepository rep;
+	
+	/**
+	 * 根据时间查询所有的关键词
+	 */
+	@Override
+	public List<KeyWordEntity> findKeyWordList(String str) {
+		return rep.findByTime(str);
+	}
+
+	/**
+	 * 保存关键词
+	 */
+	@Override
+	public boolean saveKeyWord(List<KeyWordEntity> li) {
+		Iterable<KeyWordEntity> save = rep.save(li);
+		if(save!= null){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	@Override
+	public boolean deleteData(List<KeyWordEntity> list) {
+		try {
+			rep.delete(list);
+			return true;	
+		} catch (Exception e) {
+			list.forEach(action->{
+				rep.delete(action);
+			});
+			return true;
+		}
+	}
+
+	@Override
+	public List<KeyWordEntity> findAllKeyWord() {
+		List<KeyWordEntity> all = (List<KeyWordEntity>) rep.findAll();
+		return all;
+	}
+
+}
