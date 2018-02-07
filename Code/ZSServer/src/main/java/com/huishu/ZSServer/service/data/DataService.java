27fcc69c51b9bfbc35b0c9f4e7fb3f64fbe1d103
qingyun <1348426268@qq.com>
2018-02-07 18:16:34 +0800

    慧数招商地图修改

--- Code/ZSServer/src/main/java/com/huishu/ZSServer/service/data/DataService.java
+++ Code/ZSServer/src/main/java/com/huishu/ZSServer/service/data/DataService.java
@@ -1,43 +1,43 @@
-package com.huishu.ZSServer.service.data;
-
-import com.alibaba.fastjson.JSONObject;
-import com.huishu.ZSServer.entity.Institutional;
-
-/**
- * @author hhy
- * @date 2017年11月15日
- * @Parem
- * @return 
- * 
- */
-public interface DataService {
-
-	/**
-	 * @param value
-	 * @return
-	 * 转换机构库数据
-	 */
-	Institutional transformData1(String value);
-
-	/**
-	 * @param info
-	 * @return
-	 */
-	boolean addData1(Institutional info);
-	/**
-	 * 
-	 */
-	JSONObject rederData(String value);
-	boolean  saveData(JSONObject obj);
-	//生物产业数据读取
-	JSONObject redData(String value);
-
-	/**
-	 * 生物产业数据保存
-	 * @param obj
-	 * @return
-	 */
-	boolean updateData(JSONObject obj);
-	
-	
-}
+//package com.huishu.ZSServer.service.data;
+//
+//import com.alibaba.fastjson.JSONObject;
+//import com.huishu.ZSServer.entity.Institutional;
+//
+///**
+// * @author hhy
+// * @date 2017年11月15日
+// * @Parem
+// * @return 
+// * 
+// */
+//public interface DataService {
+//
+//	/**
+//	 * @param value
+//	 * @return
+//	 * 转换机构库数据
+//	 */
+//	Institutional transformData1(String value);
+//
+//	/**
+//	 * @param info
+//	 * @return
+//	 */
+//	boolean addData1(Institutional info);
+//	/**
+//	 * 
+//	 */
+//	JSONObject rederData(String value);
+//	boolean  saveData(JSONObject obj);
+//	//生物产业数据读取
+//	JSONObject redData(String value);
+//
+//	/**
+//	 * 生物产业数据保存
+//	 * @param obj
+//	 * @return
+//	 */
+//	boolean updateData(JSONObject obj);
+//	
+//	
+//}
