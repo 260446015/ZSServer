f3abd175a98f407a8e746d438d60c4305b93eb29
anchunyu <15863193561@163.com>
2018-05-23 17:33:41 +0800

    tijiao

---
+++ Code/ManageServer/src/main/java/com/huishu/ManageServer/repository/first/RolePermissionRepository.java
@@ -0,0 +1,15 @@
+
+package com.huishu.ManageServer.repository.first;
+
+import com.huishu.ManageServer.entity.dbFirst.RolePermission;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * 权限管理
+ */
+
+public interface RolePermissionRepository extends CrudRepository<RolePermission, Long>{
+
+
+
+}
